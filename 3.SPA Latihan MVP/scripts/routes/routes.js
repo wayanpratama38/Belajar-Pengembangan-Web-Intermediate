2cf22d04d2e911fb68545d4bf6dbb4e8ec965c42
wynprtm38 <wayanpratama38@gmail.com>
2025-05-01 02:49:54 +0700

    Seperating each pages (view) and handling from routes.js

--- 3.SPA Latihan MVP/scripts/routes/routes.js
+++ 3.SPA Latihan MVP/scripts/routes/routes.js
@@ -1,37 +1,5 @@
-class HomePage {
-  async render() {
-    return `
-      <h1 class="content-title">Home Page</h1>
-      <p>Ini adalah konten halaman utama.</p>
-      <p>Mari kita cek <a href="#/about">halaman about</a>!</p>
-    `;
-  }
-
-  async afterRender() {
-    // Do something...
-  }
-}
-
-class AboutPage {
-  async render() {
-    return `
-      <h1 class="content-title">About Page</h1>
-      <p>Ini adalah konten halaman about.</p>
-      <ol>
-        <li>Kita</li>
-        <li>punya</li>
-        <li>elemen</li>
-        <li>daftar</li>
-        <li>di sini!</li>
-      </ol>
-      <p>Oke, sudah cukup. Kita bisa kembali ke <a href="#/">halaman home</a>.</p>
-    `;
-  }
-
-  async afterRender() {
-    // Do something...
-  }
-}
+import AboutPage from "../pages/about-page";
+import HomePage from "../pages/home-page";
 
 const routes = {
   '/': () => new HomePage(),
