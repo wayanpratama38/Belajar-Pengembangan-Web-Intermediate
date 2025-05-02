3631647c39f99a7f4e2c23279bfab043e469137a
wynprtm38 <wayanpratama38@gmail.com>
2025-05-02 20:53:50 +0700

    I left the auth-api model lol

--- 4.First Submission/src/scripts/data/auth-api.js
+++ 4.First Submission/src/scripts/data/auth-api.js
@@ -30,25 +30,3 @@ const AuthModel = {
 };
 
 export default AuthModel;
-
-// export async function registerUser(userData){
-//   const response = await fetch(ENDPOINTS.REGISTER,{
-//     method:"POST",
-//     headers:{
-//       "Content-Type":"application/json"
-//     },
-//     body:JSON.stringify(userData)
-//   })
-//   return response.json();
-// }
-
-// export async function loginUser(userData) {
-//   const response = await fetch(ENDPOINTS.LOGIN,{
-//     method:"POST",
-//     headers:{
-//       "Content-Type" : "application/json"
-//     },
-//     body: JSON.stringify(userData)
-//   })
-//   return response.json();
-// }
